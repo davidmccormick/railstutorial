7ba783d2f13e9b593ba693c18ae20110bb0f8856
Dave <mccormd@markhouse.localdomain>
2013-08-22 22:54:39 +0100

    catch any changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:edit, :update]
+	before_filter :correct_user, only: [:edit, :update]
+
+	def index
+		@users = User.all
+	end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -19,15 +26,31 @@ class UsersController < ApplicationController
   end
   
   def edit
-  	@user = User.find(params[:id])
+  	#removed because this is done in correct_user before_filter 
+  	#@user = User.find(params[:id])
   end
   
   def update
-  	@user = User.find(params[:id])
+  	#removed because this is done in correct_user before_filter 
+  	#@user = User.find(params[:id])
   	if @user.update_attributes(params[:user])
-  		# handle success
+  		flash[:success] = "Profile updated"
+  		sign_in @user
+  		redirect_to @user
   	else
   		render 'edit'
   	end
   end
+  
+  private
+  
+  	def signed_in_user
+  		store_location
+  		redirect_to signin_path, notice: "Please sign in." unless signed_in?
+  	end
+  	
+  	def correct_user
+  		@user = User.find(params[:id])
+  		redirect_to(root_path) unless current_user?(@user)
+  	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-	def gravatar_for(user)
+	def gravatar_for(user,options={ size: 75 })
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?r=xpg&d=identicon&s=#{options[:size]}"
 		image_tag(gravatar_url, alt: user.name, class: 'gravatar')
 	end
 end
